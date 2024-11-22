9a9ff59568b335c976156d21219e6b50666912c4
ntsolakos <ntsolakos.me@gmail.com>
2024-11-22 15:05:07 +0200

    - Update Private Relay URL

--- src/utils/gunSetup.js
+++ src/utils/gunSetup.js
@@ -3,7 +3,7 @@ import Gun from 'gun';
 const gun = Gun({
   peers: [
     {
-      url: 'https://chat-3t90.onrender.com/gun',
+      url: 'https://gun-private-relay.onrender.com/gun',
       headers: {
         'x-api-key': '940a7906641dd6bdf6fcd39eefe757248395e6cbe7b292a8f55c3a8e1fcceee5', // The same key you set in Render
       },
