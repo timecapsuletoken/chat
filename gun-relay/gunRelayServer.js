2ea4981a9dc2d76daff37076328d77485bd7e079
ntsolakos <ntsolakos.me@gmail.com>
2024-11-22 17:38:29 +0200

    - Testing Gun relay with Render

--- gun-relay/gunRelayServer.js
+++ gun-relay/gunRelayServer.js
@@ -2,28 +2,14 @@ const Gun = require('gun');
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 8765; // Use Render's assigned port or fallback
-
-// Middleware to check API key
-app.use((req, res, next) => {
-  const apiKey = process.env.GUN_PEER_API_KEY;
-  if (apiKey && req.headers['x-api-key'] !== apiKey) {
-    return res.status(403).send('Forbidden: Invalid API Key');
-  }
-  next();
-});
+const port = 8765; // You can choose any available port
 
 // Serve Gun.js and set up the relay server
 app.use(Gun.serve);
 
 const server = app.listen(port, () => {
-  console.log(`Gun relay server running on http://localhost:${port}`);
+  console.log(`Gun relay Localserver running on http://localhost:${port}`);
 });
 
-// Enable Radisk for persistent storage
-const gun = Gun({
-  web: server,
-  radisk: true, // Enables Radisk for persistent storage
-});
-
+// Initialize Gun with the server
 Gun({ web: server });
