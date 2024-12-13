bdda65a6f84c9c909c27ead36baf30a7fb8899d9
ntsolakos <ntsolakos.me@gmail.com>
2024-12-13 03:26:46 +0200

    - Update Gun.js Relay - moving `gun-relay` folder to another repo

--- src/utils/gunSetup.js
+++ src/utils/gunSetup.js
@@ -4,7 +4,7 @@ const gun = Gun({
   //peers: ['http://192.168.1.33:8765/gun'], // Local relay server
   //peers: ['https://41rk093t-8765.euw.devtunnels.ms/gun'],
   //peers: ['https://gun-manhattan.herokuapp.com/gun'], 
-  peers: ['https://gun-private-relay.glitch.me/gun'],
+  peers: ['https://optimistic-jacqui-timecapsule-93aa0913.koyeb.app/gun'],
   localStorage: false, // Disable browser localStorage
 });
 
