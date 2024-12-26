44bfa198df434c1fc1865af14b58995a240aaac9
ntsolakos <ntsolakos.me@gmail.com>
2024-12-26 21:41:34 +0200

    Edit: Adjusting `useGradientRotadingEffect` with `observer` and `Skeleton` when component is loaded to execute the function

--- src/components/LandingPage/utils/useGradientRotadingEffect.js
+++ src/components/LandingPage/utils/useGradientRotadingEffect.js
@@ -2,19 +2,28 @@ import React from 'react';
 
 export const useGradientRotadingEffect = () => {
   React.useEffect(() => {
-    const elements = document.querySelectorAll('.rotating-color');
+    const updateElements = () => {
+      const elements = document.querySelectorAll('.rotating-color');
+      elements.forEach((element, index) => {
+        let angle = 0;
 
-    elements.forEach((element, index) => {
-      let angle = 0;
+        const updateAnimation = () => {
+          angle = (angle + 1) % 360;
+          element.style.setProperty('--angle', `${angle + index * 120}deg`);
+          requestAnimationFrame(updateAnimation);
+        };
 
-      const updateAnimation = () => {
-        angle = (angle + 1) % 360;
-        element.style.setProperty('--angle', `${angle + index * 120}deg`);
+        element.style.setProperty('--angle', '0deg');
         requestAnimationFrame(updateAnimation);
-      };
+      });
+    };
 
-      element.style.setProperty('--angle', '0deg');
-      requestAnimationFrame(updateAnimation);
-    });
+    updateElements();
+
+    // Observe for new elements dynamically added to the DOM
+    const observer = new MutationObserver(updateElements);
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    return () => observer.disconnect();
   }, []);
 };
